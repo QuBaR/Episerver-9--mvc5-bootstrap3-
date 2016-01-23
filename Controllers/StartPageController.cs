b59bec9ab96abbfbceafa08e5fb758f9c7f9dda1
Robert Jansz <robert@webking.se>
2016-01-23 12:55:26 +0100

    Part 1 Episerver 9 up and running with one startpage

---
+++ Controllers/StartPageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Episerver9.Models.Pages;
+using EPiServer.Web.Mvc;
+
+namespace Episerver9.Controllers
+{
+    public class StartPageController : PageController<StartPage>
+    {
+        // GET: StartPage
+        public ActionResult Index(StartPage currentPage)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Pages/StartPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Core;
+using EPiServer.DataAnnotations;
+
+namespace Episerver9.Models.Pages
+{
+    [ContentType]
+    public class StartPage : PageData
+    {
+    }
+}
